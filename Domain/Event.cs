616e7cfd7521c7329e6d916bdd72e0c1b8a0f3b0
Lian Castro <liancastro@hotmail.com>
2023-04-02 15:55:42 -0300

    initial api commit

---
+++ Domain/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Place { get; set; }
+    }
+}
